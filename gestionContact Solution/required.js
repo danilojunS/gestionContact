d3d406aa17fca09b1e72e49cebbfd2d6de5f7509
DupeF <dupe.francis@gmail.com>
2015-02-20 00:04:51 +0100

    Ajout d'un loginListener un peu plus  complexe Ajout d'une classe "Utilisateur" au modèle de données

--- gestionContact Solution/required.js
+++ gestionContact Solution/required.js
@@ -1,6 +1,7 @@
 ﻿function myLogin(userName, password) //simple login listener
 {
-	if (userName == 'Admin')
+	var theUser = directory.internalStore.User({name: userName});
+	if (theUSer != null) //user exists  in the directory
 		return false; // allow directory authentication
 	else
 	{
@@ -30,11 +31,50 @@
 				ID: userID, //UUID (converted if necessary)
 				name: userName, //user login
 				fullName: userName, //friendly display name
-				belongsTo: theGroups,
-				storage: { test: 1, test2: "aaa" }
+				belongsTo: theGroups
 			}; //array of groups
 			return user;
 		} else
 			return {error: 1024, errorMessage: 'Invalid Login'};
 	}
 };
+
+function myLogin2(userName, password)
+{
+	var theUser = directory.internalStore.User({name: userName});
+	if (theUser != null) //user exists  in the directory
+		return false; // allow directory authentication
+	else
+	{
+		var result = {error: 1024, errorMessage: 'InvalidLogin'};
+		var contactDS = solution.getApplicationByName("gestionContact").ds;
+		var theOwner = contactDS.Owner({login:userName});
+		if(theOwner != null)
+		{
+			if(theEmployee.password === directory.computeHA1(userName,password))
+			{
+				var theGroups = [];
+				var putIntoStorage = {myId: theOwner.ID};
+				switch (theOwner.accessLevel){
+					case 1:
+						theGroups = ['Admin'];
+						break;
+					case 2:
+						theGroups = ['DataSteward'];
+						break;
+					case 3:
+						theGroups = ['BusinessAalyst'];
+						break;
+				}
+			}
+			result = {
+				ID: theOwner.ID,
+				name: theOwner.login,
+				fullName: theEmployee.firstName + '' + theEmployee.lastName,
+				belongsTo: theGroups,
+				storage: putIntoStorage};
+		}
+	}
+	return result;
+}
+					
